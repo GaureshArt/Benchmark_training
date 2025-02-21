d0906220f777deb3fe2d194969139d93c37e78dd
Gauresh Tambe <gaureshtambe25@gmail.com>
2025-02-21 11:09:51 +0530

    Some console logs removed and format

--- Assignment No 13 20_feb/TaskManagement/src/components/TaskInput.tsx
+++ Assignment No 13 20_feb/TaskManagement/src/components/TaskInput.tsx
@@ -1,69 +1,95 @@
-import { ChangeEvent,  memo,    useState } from "react"
-import { TaskInputPropsType, TaskType } from "../types/TaskTypes"
+import { ChangeEvent, memo, useState } from "react";
+import { TaskInputPropsType, TaskType } from "../types/TaskTypes";
 
-import toast from "react-hot-toast"
+import toast from "react-hot-toast";
 
+export const TaskInput = memo(({ handleAddTask }: TaskInputPropsType) => {
+  const [taskData, setTaskData] = useState<TaskType>({
+    task: "",
+    taskStatus: "",
+    id: -1,
+  });
 
-export const TaskInput  = memo(({handleAddTask}:TaskInputPropsType)=>{
-    const [taskData,setTaskData] = useState<TaskType>({
-        task:'',
-        taskStatus:'',
-        id:-1
-    })
+  const handleTaskData = (e: ChangeEvent<HTMLInputElement>) => {
+    setTaskData({
+      ...taskData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    
-console.log("TaskInout")
- 
-    const handleTaskData =(e:ChangeEvent<HTMLInputElement>)=>{
-        setTaskData({
-            ...taskData,
-            [e.target.name]:e.target.value,
-        })
-        
+  const handleValidation = (): boolean => {
+    if (!taskData.task || !taskData.task.trim()) {
+      toast.error("Please add Task Content!", {
+        duration: 1000,
+      });
+      return false;
     }
-
-    const handleValidation = ():boolean=>{
-
-        if(!taskData.task || !taskData.task.trim()){
-            toast.error('Please add Task Content!', {
-                duration: 1000,
-              });
-            return false;
-        }
-        if(!taskData.taskStatus){
-            toast.error('Please select Task Status',{
-                duration: 1000,
-              });
-            return false;
-        }
-        return true;
+    if (!taskData.taskStatus) {
+      toast.error("Please select Task Status", {
+        duration: 1000,
+      });
+      return false;
     }
-    
-    const handleTaskSubmit = ()=>{
-        taskData.id = Date.now();
-        if(!handleValidation())return;
-        handleAddTask(taskData)
-        setTaskData({
-            task:'',
-            taskStatus:'',
-            id:-1
-        })
-    }
-    return (
-        <>
-        <div className="w-2/5 h-30 border border-slate-200 rounded mt-10  grid grid-rows-3 p-2 text-slate-200">
-            <div className="row-span-1 w-full  ">
-                <label className="text-xl text-slate-200 font-mono mr-4" htmlFor="task">Task</label>
-            <input type="text" name="task" id="task" value={taskData.task}  onChange={handleTaskData} className="w-3/4 border rounded border-slate-200"/>
-            </div>
-            <div className="flex justify-start items-center gap-3">
-                <label htmlFor="Incomplete">Incomplete</label>
-                <input type="radio" name="taskStatus" id="Incomplete" onChange={handleTaskData} value={'Incomplete'} checked={taskData.taskStatus==='Incomplete'}/>
-                <label htmlFor="Complete">Complete</label>
-                <input type="radio" name="taskStatus" id="Complete" onChange={handleTaskData} value={'Complete'} checked={taskData.taskStatus==='Complete'}/>
-            </div>
-            <button type="submit" onClick={handleTaskSubmit} className="w-1/5 h-8 m-auto bg-green-400 cursor-pointer">Submit</button>
+    return true;
+  };
+
+  const handleTaskSubmit = () => {
+    taskData.id = Date.now();
+    if (!handleValidation()) return;
+    handleAddTask(taskData);
+    setTaskData({
+      task: "",
+      taskStatus: "",
+      id: -1,
+    });
+  };
+  return (
+    <>
+      <div className="w-2/5 h-30 border border-slate-200 rounded mt-10  grid grid-rows-3 p-2 text-slate-200">
+        <div className="row-span-1 w-full  ">
+          <label
+            className="text-xl text-slate-200 font-mono mr-4"
+            htmlFor="task"
+          >
+            Task
+          </label>
+          <input
+            type="text"
+            name="task"
+            id="task"
+            value={taskData.task}
+            onChange={handleTaskData}
+            className="w-3/4 border rounded border-slate-200"
+          />
         </div>
-        </>
-    )
-})
+        <div className="flex justify-start items-center gap-3">
+          <label htmlFor="Incomplete">Incomplete</label>
+          <input
+            type="radio"
+            name="taskStatus"
+            id="Incomplete"
+            onChange={handleTaskData}
+            value={"Incomplete"}
+            checked={taskData.taskStatus === "Incomplete"}
+          />
+          <label htmlFor="Complete">Complete</label>
+          <input
+            type="radio"
+            name="taskStatus"
+            id="Complete"
+            onChange={handleTaskData}
+            value={"Complete"}
+            checked={taskData.taskStatus === "Complete"}
+          />
+        </div>
+        <button
+          type="submit"
+          onClick={handleTaskSubmit}
+          className="w-1/5 h-8 m-auto bg-green-400 cursor-pointer"
+        >
+          Submit
+        </button>
+      </div>
+    </>
+  );
+});
--- Assignment No 13 20_feb/TaskManagement/src/components/TaskList.tsx
+++ Assignment No 13 20_feb/TaskManagement/src/components/TaskList.tsx
@@ -1,33 +1,51 @@
+import { memo } from "react";
+import { TaskListPropsType } from "../types/TaskTypes";
 
-
-import { memo} from "react";
-import {  TaskListPropsType } from "../types/TaskTypes";
-
-
-export const TaskList = memo(({handleRemoveTask,handleStatus,totalCompletedTask,state}:TaskListPropsType)=>{
-    
-    console.log("Tasklist comp")
-   
-       return (
-           <>{state.length?
-   
-               <div className="w-1/2 h-auto border border-slate-200 rounded-md">
-                   <ul className="flex justify-start items-center flex-col gap-4 p-2 text-slate-100">
-                       <li>Total Completed Task - {totalCompletedTask}</li>
-                       {
-                           state.map((task)=>{
-                               return (
-                                   <li key={task.id} className="border border-slate-200 rounded-md w-4/5 h-15 flex gap-5 pr-4  pl-5 items-center">
-                                       <span className="w-full text-lg">Task: {task.task}</span>
-                                       <span className={`w-1/5 h-8 rounded-lg border p-2 text-center flex justify-center items-center ${task.taskStatus==='Incomplete'?'bg-red-400':'bg-green-400'} text-slate-100 cursor-pointer`}  onClick={()=>handleStatus(task)}>{task.taskStatus}</span>
-                                       <button className="w-20 h-10 border rounded  bg-red-500 border-red-300 p-2 cursor-pointer" onClick={()=>handleRemoveTask(task)}>Remove</button>
-                                   </li>
-                               )
-                           })
-                       }
-                   </ul>
-               </div>:''
-           }
-           </>
-       )
-   })
+export const TaskList = memo(
+  ({
+    handleRemoveTask,
+    handleStatus,
+    totalCompletedTask,
+    state,
+  }: TaskListPropsType) => {
+    return (
+      <>
+        {state.length ? (
+          <div className="w-1/2 h-auto border border-slate-200 rounded-md">
+            <ul className="flex justify-start items-center flex-col gap-4 p-2 text-slate-100">
+              <li>Total Completed Task - {totalCompletedTask}</li>
+              {state.map((task) => {
+                return (
+                  <li
+                    key={task.id}
+                    className="border border-slate-200 rounded-md w-4/5 h-15 flex gap-5 pr-4  pl-5 items-center"
+                  >
+                    <span className="w-full text-lg">Task: {task.task}</span>
+                    <span
+                      className={`w-20 h-8 rounded-lg border p-2 text-center flex justify-center items-center ${
+                        task.taskStatus === "Incomplete"
+                          ? "bg-red-400"
+                          : "bg-green-400"
+                      } text-slate-100 cursor-pointer`}
+                      onClick={() => handleStatus(task)}
+                    >
+                      {task.taskStatus}
+                    </span>
+                    <button
+                      className="w-20 h-10 border rounded  bg-red-500 border-red-300 p-2 cursor-pointer"
+                      onClick={() => handleRemoveTask(task)}
+                    >
+                      Remove
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        ) : (
+          ""
+        )}
+      </>
+    );
+  }
+);
